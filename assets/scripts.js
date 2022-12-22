796b4180fc2a28f4ac4458bb340d5a1430052171
Mohamed <Aminbakayoko79@gmail.com>
2022-12-21 19:48:51 -0500

    lab 11

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,62 +1,84 @@
 const cart = {
-    currentPrice:0,
-    items: [], 
-    additem: function(cookie,price) {
+    currentPrice: 0,
+    items: [],
+    
+    addItem: function(cookie, price) {
+        // add a cookie (string) to the items array
+    this.items.push(cookie)
+        //add the price (number) to the currentPrice properties
+    this.currentPrice= this.currentPrice + price 
+    },
 
-        this.items.push(cookie)
+    clear: function() {
+        //reset the currentPrice and items properties
+        this.currentPrice= 0
+        this.items= []
+    },
 
-        this.currentPrice=this.currentPrice+price
-},
-clear:function() {
-    this.currentPrice=0
-    this.items=[]
-},
+    getSummary: function() {
+        let summary = `<p>Number of Items: ${this.items.length}</p>
+            <h4>Details</h4>
+            <p>${this.items.join('<br>')}</p>
+            <p>Total Price: $${this.currentPrice}</p>`
+        return summary
+    },
 }
+
 
 function addToCart(cookie) {
-    /*PRICES
-    peanut butter:20
-    sandies:30
-    party press:35
+    /* 
+        PRICES
+        peanut butter: 20
+        sandies: 30
+        party press: 35
+        chocolate chip: 25
     */
    
-    console.log('The user is adding this type of cookie tp their cart: ', cookie)
+   console.log('The user is adding this type of cookie to their cart: ' , cookie) 
+
+
 
    //add 1 to the itemCount variable
-
-   
-   
    //add the correct price to the currentPrice variable
+   if (cookie == 'peanut butter'){cart.addItem(cookie, 20)}
+   if (cookie == 'sandies') {cart.addItem(cookie, 30)}
+   if (cookie == 'party press') {cart.addItem(cookie, 35)}
+   if (cookie == 'chocolate chip') {cart.addItem(cookie, 25)}
 
-   if (cookie == "peanut butter") {
-        currentPrice == currentPrice + 20 
-   } else if (cookie == "sandles") {
-        currentPrice == currentPrice +30
-   } else if ( cookie ="party press"){  
-   currentPrice = currentPrice +35
-   } else if (cookie == "chocolate chip") {
-    currentPrice = currentPrice = 25
+   document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
 }
-document.querySelector(".hoverText").innerHTML = cart.currentPrice
-console.log(cart)
-document.getElementById("cartItems").innerHTML = cart.items.length
 
-}
 
 
 function checkout() {
-    console.log ('User is checking out.')
-    //let your customer know how many items they are purchasing and the price
-    window.alert(`You have a total of ${itemCount} items that will cost ${currentPrice}. Thank you for shopping with us`)
-    prompt("Please provide your name and address./n Thank you for shopping with us!")
+    console.log('User is checking out.')
+    //Let your customer know how many items they are purchasing and the price
+    window.alert(`Item Count: ${cart.items.length} Total Cost: ${cart.currentPrice}`)
+
+    document.getElementById("summary-body").innerHTML= cart.getSummary()
+    document.getElementById("summary").style.display= "block"
+    
     cart.clear()
     document.querySelector(".hoverText").innerHTML = cart.currentPrice
     console.log(cart)
-    document.getElementById("cartItems").innerHTML = cart.items.length
+    document.getElementById("cartItems").innerHTML= cart.items.length
 }
 
-function darkmode() {
-    document.querySelector("body").style.backgroundColor = "black"
-    document.querySelector("main").style.color = "white"
-    document.querySelector("h1").style.color = "white"
+
+function darkMode() {
+	document.querySelector("body").style.backgroundColor="navy"
+    document.querySelector("body").style.color="white"
 }
+
+
+function clearCart() {
+    cart.clear()
+    document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
+}
+
+clearcart.addEventListener("mousedown", clearCart)
+//the first clearcart above is the id of the clear cart button in index.html
